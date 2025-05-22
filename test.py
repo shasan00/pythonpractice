5b26ad00b6901206e0d05974ac59fea3cd118056
shasan00 <shasan3499@gmail.com>
2025-05-22 13:53:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
-def studentGrades(name, grade):
-    gradebook = dict(zip(name, grade))
-    query = input("Enter a student name: ").strip()
 
-    if query in gradebook:
-        return gradebook[query]
-    else:
-        return "Student not found."
-
-studentGrades(["syed", "santosh", "angela"], [90, 20, 30])
